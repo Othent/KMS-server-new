eaa7c7f839c91cebb060c47bb075605391d8a3c0
Dani Gámez Franco <gmzcodes@outlook.com>
2024-09-12 15:13:35 +0200

    Fix test setup.

--- src/lib/utils/kms/kmsClient.ts
+++ src/lib/utils/kms/kmsClient.ts
@@ -11,27 +11,31 @@ if (KMS_ENVIRONMENT === "DEVELOPMENT_SERVER" || KMS_ENVIRONMENT === "PRODUCTION_
     credentials: CONFIG.GOOGLE_CREDENTIALS,
   });
 
-  setTimeout(() => {
-    const warningMessage = `!!  ⚠️  Using a real (${ KMS_ENVIRONMENT }) Google KMS server!  !!`;
-    const warningMessageSeparators = "!".repeat(warningMessage.length - 1);
-    const YELLOW = "\x1b[33m";
-    const RED = "\x1b[31m";
-    const RESET = "\x1b[0m";
-    const WARNING_COLOR = KMS_ENVIRONMENT.startsWith("DEVELOPMENT") ? YELLOW : RED;
+  if (process.env.NODE_ENV !== "test") {
+    setTimeout(() => {
+      const warningMessage = `!!  ⚠️  Using a real (${ KMS_ENVIRONMENT }) Google KMS server!  !!`;
+      const warningMessageSeparators = "!".repeat(warningMessage.length - 1);
+      const YELLOW = "\x1b[33m";
+      const RED = "\x1b[31m";
+      const RESET = "\x1b[0m";
+      const WARNING_COLOR = KMS_ENVIRONMENT.startsWith("DEVELOPMENT") ? YELLOW : RED;
 
-    console.log(`${ WARNING_COLOR }${ warningMessageSeparators }`);
-    console.log(warningMessage);
-    console.log(`${ warningMessageSeparators }${ RESET }`);
-    console.log();
-  }, 1000);
+      console.log(`${ WARNING_COLOR }${ warningMessageSeparators }`);
+      console.log(warningMessage);
+      console.log(`${ warningMessageSeparators }${ RESET }`);
+      console.log();
+    }, 1000);
+  }
 } else if (KMS_ENVIRONMENT === "LOCAL_MOCK") {
   const localKeyManagementServiceClient = new LocalKeyManagementServiceClient();
 
   kmsClient = localKeyManagementServiceClient;
 
-  setTimeout(() => {
-    localKeyManagementServiceClient.testLocalKeyManagementServiceClient();
-  }, 1000);
+  if (process.env.NODE_ENV !== "test") {
+    setTimeout(() => {
+      localKeyManagementServiceClient.testLocalKeyManagementServiceClient();
+    }, 1000);
+  }
 } else {
   throw new Error("Cannot initialize `kmsClient`.");
 }
